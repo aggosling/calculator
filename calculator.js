edb7acad40a7cc1e4c42610b3cfdb75d3f6ab1eb
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-06-13 22:13:48 +0100

    Add operator function and more styling.

--- calculator.js
+++ calculator.js
@@ -13,3 +13,30 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let num1 = "";
+let operator = "";
+let num2 = "";
+
+function operate(a, operator, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "X":
+            return multiply(a, b);
+        case "%":
+            return divide(a, b);
+    }
+}
+
+const numberButtons = document.querySelectorAll(".button.number");
+numberButtons.forEach(button => button.addEventListener('click', function (e) {
+    let num = e.target.innerText;
+    if (!operator)
+        num1 = num1 + num;
+    else
+        num2 = num2 + num;
+    console.log(typeof num + "//" + operator + "//" + num2);
+}));
